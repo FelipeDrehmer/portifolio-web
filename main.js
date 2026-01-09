6f6513c9ea491cf5dc20f4696083a686d08e0163
Felipe Drehmer <felipeka.drehmer@gmail.com>
2026-01-09 09:35:58 -0300

    Adicionado texto animado ao subtitulo

--- main.js
+++ main.js
@@ -1,5 +1,53 @@
-const loadPage = () => {
+class TxtType {
+    constructor(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.isDeleting = false;
+        this.tick();
+    }
 
+    tick() {
+        const i = this.loopNum % this.toRotate.length;
+        const fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.textContent = this.txt;
+        const wrap = this.el.querySelector('.wrap');
+        if (wrap) wrap.textContent = this.txt;
+
+        let delta = 200 - Math.random() * 100;
+        if (this.isDeleting) delta /= 2;
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+
+        setTimeout(() => this.tick(), delta);
+    }
+}
+
+const loadPage = () => {
+    const elements = document.querySelectorAll('.typewrite');
+    elements.forEach(el => {
+        const toRotate = el.getAttribute('data-type');
+        const period = el.getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(el, JSON.parse(toRotate), period);
+        }
+    });
 };
 
 document.addEventListener('DOMContentLoaded', () => {
